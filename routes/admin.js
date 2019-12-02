5710e22a256ef598c9add1a49fd93e10d214e6f6
Murshid_ <murshidpc@gmail.com>
2019-12-02 17:13:55 +0530

    Special Enhancement In App

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
+const path = require('path');
+
 const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = require('../controllers/products');
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-router.get('/add-product', products.addProducts);
-
-router.post('/add-product', products.postAddProducts);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,19 @@
-const express = require('express');
 const path = require('path');
 
+const express = require('express');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
-const products = require('../controllers/products');
 
-router.get('/', products.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
